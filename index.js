dfbd7fa7a883f8a704fbf2b10f2c3f245324dc2a
andregorni <ac.gorni@gmail.com>
2021-05-17 00:16:26 -0300

    reparo exerc 2.0

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-for(let contador = 1; contador <= 10; contador++){
+for (let contador = 1; contador <= 10; contador++) {
     console.log(contador)
 }
 
 
 
-let cont = 11;
+let cont = 10;
 
-while(cont >1){
-     
+while (cont >= 1) {
     console.log(cont)
-}    
+    cont--
+}
 
 
 
-for(let somaimp = 1; somaimp <= 100; somaimp+=2){
+for (let somaimp = 1; somaimp <= 100; somaimp += 2) {
     console.log(somaimp)
+}
 
 
-
-for(let somapar = 0; somapar <= 100; somapar+=2){
+for (let somapar = 0; somapar <= 100; somapar += 2) {
     console.log(somapar)
 }
