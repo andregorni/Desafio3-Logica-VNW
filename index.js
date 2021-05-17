85a7b7f4ddda4234b1f5ea1db493c86e952b7de1
andregorni <ac.gorni@gmail.com>
2021-05-16 23:32:09 -0300

    Desafio 3 - Logica - VNW

---
+++ index.js
@@ -0,0 +1,23 @@
+for(let contador = 1; contador <= 10; contador++){
+    console.log(contador)
+}
+
+
+
+let cont = 11;
+
+while(cont >1){
+     
+    console.log(cont)
+}    
+
+
+
+for(let somaimp = 1; somaimp <= 100; somaimp+=2){
+    console.log(somaimp)
+
+
+
+for(let somapar = 0; somapar <= 100; somapar+=2){
+    console.log(somapar)
+}
